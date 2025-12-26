40b616198811eef6d4827de680f1832374922250
devasher <hlxstc680@gmail.com>
2025-12-26 17:30:42 +0800

    chore(playground): remove unused varibale

--- apps/playground/src/components/Playground/CodeDisplay.tsx
+++ apps/playground/src/components/Playground/CodeDisplay.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { usePlaygroundConfig, type CodeStep } from '../../config/usePlaygroundConfig'
+import { usePlaygroundConfig } from '../../config/usePlaygroundConfig'
 import { ChevronUp, ChevronDown, Copy, Check } from 'lucide-react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
